c6cea3d9eaea3e6ea4d63a928e7e61b47583537d
sunahiri25 <104315103+sunahiri25@users.noreply.github.com>
2022-04-25 00:16:26 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,14 +1,14 @@
 const textConfig = {
   text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa:3333",
+  text2: "Tớ có điều này muốn hỏi, cậu nhớ phải trả lời thật lòng nhaaa:3333",
   text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm ny tớ rùi đó nha :v",
+  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm ny tớ gòi đó nha :v",
   text5: "Cậu mơ à???",
   text6: "Yêu ơi là yêu <3",
   text7: "lí do cậu thích tớ đi :vvvv",
   text8: "Gửi cho tớ <3",
   text9: "Vì cthươngthương vlll:>>",
-  text10: "Tớ biết mà ^^ Yêu cậux 1000.000",
+  text10: "Tớ biết mà ^^ Yêu cậu x1000.000",
   text11:
     "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
   text12: "Okii lunn <3",
